611b1fd2636f9210f3f14faa1451b3e36d86401a
Arghya Datta <arghya@swamicompute.wustl.edu>
2018-12-08 13:29:25 -0600

    preprocess working

--- scripts/aux_funcs.py
+++ scripts/aux_funcs.py
@@ -10,5 +10,21 @@ def generate_team_stats(clubs_dict, squad_list,squad_name,midfield,defense,attac
         else: tier = 3
         d.append({'Club': key, 'Rating': best_rating, 'Squad': best_squad, 'Defence': def_rating, 'Midfield': mid_rating, 'Attack': att_rating, 'Formation': best_formation,'Tier': tier})
     processed_data = pd.DataFrame(d).reindex_axis(['Club', 'Squad', 'Formation', 'Tier', 'Rating', 'Attack', 'Midfield', 'Defence'], axis = 1).set_index('Club')
-    #processed_data.to_csv('data/WC18_processed.csv')
+    processed_data.to_csv('data/epl_processed.csv')
     return processed_data
+
+def generate_prior_data(stats,epl_clubs):
+    d = []
+    for cA in qualified_countries:
+        for countryB in qualified_countries:
+            if countryA == countryB: continue
+            try:
+                xA = np.mean(map(float, prior[(prior['home_team'] == countryA) & (prior['away_team'] == countryB)]['home_score'].to_string(index = False).split('\n')) + map(float, prior[(prior['home_team'] == countryB) & (prior['away_team'] == countryA)]['away_score'].to_string(index = False).split('\n'))).round(2)
+                xB = np.mean(map(float, prior[(prior['home_team'] == countryB) & (prior['away_team'] == countryA)]['home_score'].to_string(index = False).split('\n')) + map(float, prior[(prior['home_team'] == countryA) & (prior['away_team'] == countryB)]['away_score'].to_string(index = False).split('\n'))).round(2)
+            except:
+                xA = int(stats[stats.index == countryB]['Tier'].get_values())
+                xB = int(stats[stats.index == countryA]['Tier'].get_values())
+            d.append({'CountryA': countryA, 'CountryB': countryB, 'meanScoreA': xA, 'meanScoreB': xB})
+    processed_data = pd.DataFrame(d).reindex_axis(['CountryA', 'meanScoreA', 'CountryB', 'meanScoreB'], axis = 1).set_index(['CountryA', 'CountryB'])
+    processed_data.to_csv('data/WC18_prior.csv')
+
